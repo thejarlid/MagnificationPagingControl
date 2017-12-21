873ad9f61022acd7417366eae84adb8ef4901894
Dilraj Devgun <dilrajsinghdevgun@hotmail.co.uk>
2017-12-20 23:47:42 -0800

    initial commit

---
+++ Moleskine Paging Control/MagnificationPagingControl.swift
@@ -0,0 +1,282 @@
+//
+//  MagnificationPagingControl.swift
+//  Moleskine Paging Control
+//
+//  Created by Dilraj Devgun on 12/20/17.
+//  Copyright © 2017 Dilraj Devgun. All rights reserved.
+//
+//  A control that display's a vertical series of dots which the
+//  user can then pan over and a magnification and selection effect
+//  is created.
+//
+//    MIT License
+//
+//    Copyright (c) 2017 Dilraj Devgun
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+
+import UIKit
+
+protocol MagnificationPagingControlDelegate {
+    /**
+     Called when the user first makes contact with the control
+     
+     - parameter point: The point in the control's coodinate space that the user pressed
+    */
+    func touchDownInPageControl(point:CGPoint)
+    
+    /**
+     Called when the touch in the control was cancelled
+    */
+    func touchCancelledInPageControl()
+    
+    /**
+     Called when the touch in the control ended
+     */
+    func touchEndedInPageControl()
+    
+    /**
+     Called when the touch in the control failed
+     */
+    func touchFailedInPageControl()
+    
+    /**
+     Called when the user changes the index of the control
+     
+     - parameter index: the current index of the control
+     */
+    func pageControlChangedToIndex(index:Int)
+    
+    /**
+     Called when the colour of the dot at an index is needed
+     
+     - parameter index: the index that the colour is needed for
+     - returns: the colour for the requested index
+     */
+    func colourForDotAtIndex(index:Int) -> UIColor
+}
+
+class MagnificationPagingControl: UIView {
+    var delegate:MagnificationPagingControlDelegate?
+    private var currentIndex:Int = -1
+    private var numDots:Int = 0
+    private var circleDiameter:CGFloat!
+    private var circleSpacing:CGFloat!
+    private var circles:[UIView] = []
+    private var generator:UISelectionFeedbackGenerator?
+    private var originalFrame:CGRect!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.originalFrame = frame
+        setup()
+    }
+    
+    init(frame:CGRect, numberOfDots:Int) {
+        super.init(frame: frame)
+        self.numDots = numberOfDots
+        self.originalFrame = frame
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        // sets up size for the circles to fit numDots within the frame
+        circleDiameter = (self.frame.height * 0.5)/CGFloat(numDots)
+        circleSpacing = (self.frame.height * 0.5)/CGFloat(numDots-1)
+        
+        // creates the gesture recognizer used to respond to a user's touch in the control space
+        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleTouchInContainer(gesture:)))
+        gesture.minimumPressDuration = 0
+        gesture.numberOfTouchesRequired = 1
+        self.addGestureRecognizer(gesture)
+        
+        setupInitialCircles()
+    }
+    
+    /**
+     Sets up numDots dots within the frame vertically
+    */
+    private func setupInitialCircles() {
+        // make circles
+        for i in 0..<numDots {
+            let frame = CGRect(x: self.frame.width/2 - circleDiameter/2, y: (self.circleDiameter + self.circleSpacing)*CGFloat(i),
+                               width: circleDiameter, height: circleDiameter)
+            let circle = UIView(frame: frame)
+            circle.layer.cornerRadius = circleDiameter/2
+            
+            var colour = UIColor.orange
+            if let d = self.delegate {
+                colour = d.colourForDotAtIndex(index: i)
+            }
+            circle.layer.borderColor = colour.cgColor
+            circle.layer.borderWidth = 2
+            
+            self.addSubview(circle)
+            circles.append(circle)
+        }
+    }
+    
+    /**
+     Responds to a user's touch based on the state that the gesture is currently in.
+     Alerts the delegate of any notable events they may want to listen to
+     
+     - parameter gesture: the gesture sending the message
+    */
+    @objc
+    private func handleTouchInContainer(gesture:UIGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            generator = UISelectionFeedbackGenerator()
+            generator?.prepare()
+            draggedInControl(point: gesture.location(in: self))
+            if let d = self.delegate {
+                d.touchDownInPageControl(point: gesture.location(in: self))
+            }
+            break
+        case .cancelled:
+            generator = nil
+            resetCircles()
+            if let d = self.delegate {
+                d.touchCancelledInPageControl()
+            }
+            break
+        case .changed:
+            draggedInControl(point: gesture.location(in: self))
+            break
+        case .ended:
+            generator = nil
+            resetCircles()
+            if let d = self.delegate {
+                d.touchEndedInPageControl()
+            }
+            break
+        case.failed:
+            if let d = self.delegate {
+                d.touchFailedInPageControl()
+            }
+            resetCircles()
+            generator = nil
+            break
+        default:
+            generator = nil
+            resetCircles()
+            print("failed")
+        }
+    }
+    
+    /**
+     Sets the current index of the control, filling the dot and alerting the
+     delegate of the index change
+     
+     Ignores any value where index is negative or >= the total number of dots in the control
+     
+     - parameter index: the index to change to
+    */
+    func setCurrentIndex(index:Int) {
+        if index < 0 || index >= self.numDots {
+            return
+        }
+        self.currentIndex = index
+        var colour = UIColor.orange
+        if let d = self.delegate {
+            colour = d.colourForDotAtIndex(index: index)
+            d.pageControlChangedToIndex(index: currentIndex)
+        }
+        self.circles[index].backgroundColor = colour
+    }
+    
+    /**
+     Getter for the current index of the control
+     - return: the currently selected index in the control, -1 if nothing is selected
+    */
+    func getCurrentIndex() -> Int {
+        return self.currentIndex
+    }
+    
+    /**
+     Responds to the user dragging within the view. Handles circle growing and setting the current index if it changes.
+     Alerts the delegate of any index changes which may occur
+     
+     - parameter point: the current position of the user's finger in the control's coordinate
+    */
+    private func draggedInControl(point:CGPoint) {
+        // finds the current dot that the user's finger is within bounds of
+        // first gets the start of the circles within the containing view
+        // then finds the displacement between the finger and the start and divides by the height of a circle's bounds
+        // then bounds that circle between 0 and numDots - 1
+        let halfFrameHeight = self.frame.height/2
+        let circleStart = halfFrameHeight - ((self.circleDiameter * CGFloat(numDots)) + (CGFloat(numDots-1) * self.circleSpacing))/2
+        let segmentHeight = self.circleDiameter + self.circleSpacing
+        let index = min(max(Int(max(0, (point.y - circleStart))/segmentHeight), 0), numDots-1)
+        
+        // if there was a previously selected index that is different from the current index, we clear the fill
+        if currentIndex != -1 && currentIndex != index {
+            circles[currentIndex].backgroundColor = UIColor.clear
+        }
+        
+        // set the fill on the currently selected dot
+        var colour = UIColor.orange
+        if let d = self.delegate {
+            colour = d.colourForDotAtIndex(index: index)
+        }
+        circles[index].backgroundColor = colour
+        
+        // alert the user of an index change if one occurs
+        if index != currentIndex {
+            generator?.selectionChanged()
+            currentIndex = index
+            if let d = self.delegate {
+                d.pageControlChangedToIndex(index: currentIndex)
+            }
+        }
+        
+        // changes the circle heights based on the distance from the dot
+        var runningHeight:CGFloat = circleStart
+        let maxDistance = self.circleDiameter*1.5 + self.circleSpacing
+        for i in 0..<numDots {
+            let distanceToCircle = min(abs((runningHeight + self.circleDiameter/2) - point.y), maxDistance)
+            let ratio = 1 + 2.5*(1  - (distanceToCircle/maxDistance))  // the 2.5 will control the size of the dot when it grows
+            let dimension = circleDiameter * ratio
+            let circle = circles[i]
+            let frame = CGRect(x: self.frame.width/2 - dimension/2, y: runningHeight, width: dimension, height: dimension)
+            circle.frame = frame
+            circle.layer.cornerRadius = dimension/2
+            runningHeight += self.circleSpacing + dimension
+        }
+    }
+    
+    /**
+     Resets the circles back to their default position, leaving the currently selected dot filled
+    */
+    func resetCircles() {
+        var i:Int = 0
+        for circle in circles {
+            let frame = CGRect(x: self.originalFrame.width/2 - circleDiameter/2, y: (self.circleDiameter + self.circleSpacing)*CGFloat(i),
+                               width: circleDiameter, height: circleDiameter)
+            circle.layer.cornerRadius = circleDiameter/2
+            circle.frame = frame
+            i+=1
+        }
+    }
+}
---
+++ Moleskine Paging Control/ViewController.swift
@@ -0,0 +1,159 @@
+//
+//  ViewController.swift
+//  Moleskine Paging Control
+//
+//  Created by Dilraj Devgun on 12/20/17.
+//  Copyright © 2017 Dilraj Devgun. All rights reserved.
+//
+//  Demo ViewController showing how to construct a Magnification
+//  Paging Control and how to respond to its delegate methods.
+//  A MagnificationPagingControl can also be created using
+//  a storyboard by dragging a UIView and changing the class
+//
+//    MIT License
+//
+//    Copyright (c) 2017 Dilraj Devgun
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+
+import UIKit
+
+class ViewController: UIViewController, MagnificationPagingControlDelegate {
+    
+    var pagingControl:MagnificationPagingControl!
+    var label:UILabel!          // displays the currently selected index of the control
+    var startWidth:CGFloat!     // starting width of the control
+    var startHeight:CGFloat!    // starting height of the control
+    var colourSwitch:UISwitch!  // switch to change the colour of the app
+    var creditLabel:UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // create the label in the center of the screen and the credit label in the bottom right hand corner
+        label = UILabel(frame: self.view.frame)
+        label.textAlignment = .center
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 28)
+        label.text = "0"
+        
+        creditLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        creditLabel.textAlignment = .right
+        creditLabel.textColor = UIColor.black
+        creditLabel.font = UIFont.systemFont(ofSize: 8)
+        creditLabel.text = "made by Dilraj Devgun"
+        creditLabel.sizeToFit()
+        creditLabel.frame = CGRect(x: self.view.frame.width - creditLabel.frame.width - 8, y: (self.view.frame.height * 0.9) + 27,
+                                   width: creditLabel.frame.width, height: creditLabel.frame.height)
+        
+        // makes the UISwitch in the bottom left hand corner
+        colourSwitch = UISwitch()
+        colourSwitch.onTintColor = UIColor.black
+        colourSwitch.isOn = true
+        colourSwitch.frame = CGRect(x: 10, y: self.view.frame.height * 0.9, width: 40, height: 30)
+        colourSwitch.addTarget(self, action: #selector(switchChanged(sender:)), for: .valueChanged)
+        
+        // sets up the control at the centre right side of the screen and sets the currently selected index to 0
+        // setting the index is optional, default behaviour is nothing selected
+        startWidth = self.view.frame.width*0.1
+        startHeight = self.view.frame.height*0.1
+        pagingControl = MagnificationPagingControl(frame: CGRect(x: self.view.frame.width - startWidth,
+                                                                 y: self.view.frame.height/2 - startHeight/2,
+                                                                 width: startWidth, height: startHeight), numberOfDots:4)
+        pagingControl.delegate = self
+        pagingControl.setCurrentIndex(index: 0)
+        
+        // adds all the created views above to the ViewController's main view
+        self.view.addSubview(creditLabel)
+        self.view.addSubview(colourSwitch)
+        self.view.addSubview(label)
+        self.view.addSubview(pagingControl)
+    }
+    
+    /**
+     Responds to the UISwitch changing its value
+     
+     - parameter sender: The switch whose value changed
+    */
+    @objc
+    func switchChanged(sender:UISwitch) {
+        var textColour = UIColor.black
+        var backgroundColour = UIColor.white
+        if !sender.isOn {
+            textColour = UIColor.white
+            backgroundColour = UIColor.black
+        }
+        self.view.backgroundColor = backgroundColour
+        self.label.textColor = textColour
+        self.creditLabel.textColor = textColour
+    }
+    
+    // MARK: - MagnificationPagingControlDelegate
+    func touchDownInPageControl(point: CGPoint) {
+        // animates the control inwards to be more dynamic and so the user's finger doesn't cover the control
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.1,
+                       options: [.beginFromCurrentState], animations: {() in
+            let frame = CGRect(x: self.view.frame.width - self.startWidth*2, y: self.view.frame.height/2 - self.startHeight/2,
+                               width: self.startWidth, height: self.startHeight)
+            self.pagingControl.frame = frame
+        }, completion: nil)
+            
+    }
+    
+    func touchFailedInPageControl() {
+        animateControlBack()
+    }
+    
+    func touchEndedInPageControl() {
+        animateControlBack()
+    }
+    
+    func touchCancelledInPageControl() {
+        animateControlBack()
+    }
+    
+    /**
+     Moves the control back into its starting position
+     */
+    func animateControlBack() {
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.1,
+                       options: [.beginFromCurrentState], animations: {() in
+            let frame = CGRect(x: self.view.frame.width - self.startWidth, y: self.view.frame.height/2 - self.startHeight/2,
+                               width: self.startWidth, height: self.startHeight)
+            self.pagingControl.frame = frame
+        }, completion: nil)
+    }
+    
+    func pageControlChangedToIndex(index: Int) {
+        label.text = "\(index)"
+    }
+    
+    func colourForDotAtIndex(index: Int) -> UIColor {
+        return UIColor.orange
+    }
+}
+
